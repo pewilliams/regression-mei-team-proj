dff0e22fbcad4e3d2b92ad6823ae991445b9f954
Osman Ghandour <oaghandour@hotmail.com>
2020-03-27 21:50:11 -0400

    data input update

--- data_manipulation/data_manipulation.R
+++ data_manipulation/data_manipulation.R
@@ -1,5 +1,5 @@
 data <- read.table(file = "C:/Users/oagha/Desktop/isye6414/regression-mei-team-proj/data_manipulation/alldata_nolabels.csv", sep=",")
-colnames(data) <- c("Y1", "Y2", "Y3","X1","X2","X3", "X4","X5","X6") 
+colnames(data) <- c("Y1", "Y2", "Y3","X1","X2","X3", "X4","X5","X6", "X7") 
 
 #Y1 = suicide_male_and_female
 #Y2 = suicide_male
@@ -7,6 +7,7 @@ colnames(data) <- c("Y1", "Y2", "Y3","X1","X2","X3", "X4","X5","X6")
 #X1 = Current_health_expenditure_percentage_of_GDP
 #X2 = female_male_labor_participation
 #X3 = gdp/capita ppp
-#X4 = physicians_per_1000_population
-#X5 = litres_alcohol_per_capita
-#X6 = Suicide_prevention_strategy, binary
+#X4 = litres_alcohol_per_capita
+#X5 = Suicide_prevention_strategy, binary
+#X6 = Psychiatrists working in mental health sector (per 100 000 population)
+#X7 = Mental hospitals (per 100 000 population)
