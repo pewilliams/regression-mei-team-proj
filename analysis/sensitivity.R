b6ce74489e2ce7c52e16a66e5e945e67cac3d93f
Peter Williams <peter.williams@catalina.com>
2020-04-10 17:08:57 -0400

    adding sensitivity plots

---
+++ analysis/sensitivity.R
@@ -0,0 +1,161 @@
+#!/usr/local/bin/R
+# Run the following to create images: 
+# Rscript analysis/sensitivity.R
+
+# cd ~/Projects/regression-mei-team-proj
+#########################
+# author: @pewilliams
+
+#################
+#helpers
+require(data.table)
+require(MASS)
+require(ggplot2)
+options(scipen = 100)
+
+bc_transform <- function(lambda,y){
+	if(lambda == 0){
+		log(y)	
+	}else{
+		(y^lambda - 1)/lambda
+	}	
+}
+
+bc_inv <- function(lambda,x){
+	if(lambda == 0){
+		exp(x)		
+	}else{
+		(lambda*x + 1)^(1/lambda)
+	}
+}
+
+build_seq <- function(x,nlen){
+	return(seq(min(x), max(x), length.out = nlen))
+}
+
+lambda <- 0.4 #chosen already
+
+#fm_labor 	#X2 = female_male_labor_participation 				
+#gdp 		#X3 = gdp/capita ppp 								
+#lalc 		#X4 = litres_alcohol_per_capita 					
+#sstrat		#X5 = Suicide_prevention_strategy, binary 			
+
+
+#### Import model and grab original data 
+#### from model object
+#final model object
+fmod <- readRDS('analysis/final_mod.rds')
+
+#data.frame associated with model
+dat <- fmod$model
+dat <- data.table(dat)
+setnames(dat, names(dat), c('srate','fm_labor',
+	'gdp','lalc','sstrat', 'wtg'))
+fmod <- lm(srate ~ fm_labor + gdp + lalc + sstrat, 
+	weights = wtg, data = dat)
+
+#prediction data.frame and sensitivity by variable
+
+#X2: Labor Participation (fm_labor)
+labor_dat <- data.frame(
+	fm_labor = build_seq(dat$fm_labor, 50),
+	gdp = mean(dat$gdp),
+	lalc = mean(dat$lalc),
+	sstrat = 1)
+
+pred <- data.frame(bc_inv(0.4, predict(fmod, 
+	newdata = labor_dat, 
+	interval = 'confidence')))
+
+labor_dat <- cbind(labor_dat, pred)
+
+lplot <- ggplot(labor_dat, 
+	aes(x = fm_labor, 
+	y = fit)) + 
+	geom_line() + 
+		xlab('Labor Participation Rate') +
+		ylab('Predicted Suicide Rate') +
+	geom_line(aes(x = fm_labor, y = upr, 
+		colour = 'red')) + 
+	geom_line(aes(x = fm_labor, y = lwr, 
+		colour = 'red')) + 
+	theme_bw() + theme(legend.position = "none")
+
+ggsave('images/labor_plot.pdf', plot = lplot)
+
+#X3: GDP 
+gdp_dat <- data.frame(
+	fm_labor = mean(dat$fm_labor),
+	gdp = build_seq(dat$gdp, 50),
+	lalc = mean(dat$lalc),
+	sstrat = 1)
+
+pred <- data.frame(bc_inv(0.4, predict(fmod, 
+	newdata = gdp_dat, 
+	interval = 'confidence')))
+
+gdp_dat <- cbind(gdp_dat, pred)
+
+gplot <- ggplot(gdp_dat, 
+	aes(x = gdp, 
+	y = fit)) + 
+	geom_line() + 
+		xlab('GDP') +
+		ylab('Predicted Suicide Rate') +
+	geom_line(aes(x = gdp, y = upr, 
+		colour = 'red')) + 
+	geom_line(aes(x = gdp, y = lwr, 
+		colour = 'red')) + 
+	theme_bw() + theme(legend.position = "none")
+
+ggsave('images/gdp_plot.pdf', plot = gplot)
+
+#X4: Litres Alcohol 
+alc_dat <- data.frame(
+	fm_labor = mean(dat$fm_labor),
+	gdp = mean(dat$gdp, 50),
+	lalc = build_seq(dat$lalc, 50),
+	sstrat = 1)
+
+pred <- data.frame(bc_inv(0.4, predict(fmod, 
+	newdata = alc_dat, 
+	interval = 'confidence')))
+
+alc_dat <- cbind(alc_dat, pred)
+
+aplot <- ggplot(alc_dat, 
+	aes(x = lalc, 
+	y = fit)) + 
+	geom_line() + 
+		xlab('Litres of Alcohol Consumer Per Year') +
+		ylab('Predicted Suicide Rate') +
+	geom_line(aes(x = lalc, y = upr, 
+		colour = 'red')) + 
+	geom_line(aes(x = lalc, y = lwr, 
+		colour = 'red')) + 
+	theme_bw() + theme(legend.position = "none")
+
+ggsave('images/alc_plot.pdf', plot = aplot)
+
+#X5: Suicide Strategy 
+ss_dat <- data.frame(
+	fm_labor = mean(dat$fm_labor),
+	gdp = mean(dat$gdp, 50),
+	lalc = mean(dat$lalc),
+	sstrat = c(1,0))
+
+pred <- data.frame(bc_inv(0.4, predict(fmod, 
+	newdata = ss_dat, 
+	interval = 'confidence')))
+
+ss_dat <- cbind(ss_dat, pred)
+
+splot <- ggplot(ss_dat, 
+	aes(y = fit, ymin = lwr, 
+	ymax = upr, x = factor(sstrat))) +
+	geom_crossbar(width = 0.2, fill = 'gray') +
+		xlab('National Suicide Prevention Strategy (1 = Yes, 0 = No)') +
+		ylab('Predicted Suicide Rate')  + 
+	theme_bw() + theme(legend.position = "none")
+
+ggsave('images/ss_plot.pdf', plot = splot)
